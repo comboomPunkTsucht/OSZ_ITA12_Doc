f1e7e4a575bc2296154176d1bcda30c1d883829d
mcpeapsUnterstrichHD <mcpeaps_HD@outlook.com>
2023-03-30 17:24:52 +0200

    .

--- Swift_App/ITA 12 Docs OSZimt/ITA 12 Docs OSZimt/ContentView.swift
+++ Swift_App/ITA 12 Docs OSZimt/ITA 12 Docs OSZimt/ContentView.swift
@@ -59,187 +59,215 @@ struct ContentView: View {
     
 }
 struct WebView: UIViewRepresentable {
+    @Binding var webView: WKWebView
     let request: URLRequest
     
     func makeUIView(context: Context) -> WKWebView {
-        return WKWebView()
-    }
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        uiView.load(request)
+        webView.load(request)
+        return webView
     }
     
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
 }
 
 struct ClassSideView: View {
-    @State private var webAddress = "https://ita12docoszimt.serveblog.net/"
+    @State private var webView = WKWebView()
+    let startURL = URL(string: "https://ita12docoszimt.serveblog.net/")!
     var body: some View {
         VStack{
-            
-            WebView(request: URLRequest(url: URL(string: webAddress)!))
-            /*HStack{
-                
+            WebView(webView: $webView, request: URLRequest(url: startURL))
+            HStack {
                 Spacer()
-                Button(action: {
-                    webViewStateModel.pageTitle = webAddress
-                    webViewStateModel.goToPage = true
-                }, label: {
-                    Image(systemName: "house.circle")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color("AccentColor"))
-                        .frame(width: 20.0)
-                })
-                .font(.title)
-                Button(action: {
-                    webViewStateModel.goBack = true
-                }, label: {
-                    Image(systemName: "arrow.backward")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color("AccentColor"))
-                        .frame(width: 20.0)
-                })
-                Button(action: {
-                    webViewStateModel.goForward = true
-                }, label: {
-                    Image(systemName: "arrow.forward")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color("AccentColor"))
-                        .frame(width: 20.0)
-                })
-                
-            }
-            .aspectRatio(contentMode: .fit) */
-        }
-        
-    }
-}
-    struct MoodleView: View {
-        @State private var webAddress = "https://moodle.oszimt.de/"
-        var body: some View {
-            VStack{
-                WebView(request: URLRequest(url: URL(string: webAddress)!)) /*
-                HStack{
-                    Spacer()
-                    Button(action: {
-                        webViewStateModel.pageTitle = webAddress
-                        webViewStateModel.goToPage = true
-                    }, label: {
-                        Image(systemName: "house.circle")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .foregroundColor(Color("AccentColor"))
-                            .frame(width: 20.0)
-                    })
-                    .font(.title)
-                    Button(action: {
-                        webViewStateModel.goBack = true
-                    }, label: {
-                        Image(systemName: "arrow.backward")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .foregroundColor(Color("AccentColor"))
-                            .frame(width: 20.0)
-                    })
-                    Button(action: {
-                        webViewStateModel.goForward = true
-                    }, label: {
-                        Image(systemName: "arrow.forward")
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .foregroundColor(Color("AccentColor"))
-                            .frame(width: 20.0)
-                    })
-                }
-                .aspectRatio(contentMode: .fit) */
-            }
+                            Button(action: goBack) {
+                                Image(systemName: "arrowshape.turn.up.left.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.black)
+                                    .frame(width: 20.0)
+                            }
+                            Button(action: goForward) {
+                                Image(systemName: "arrowshape.turn.up.right.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.black)
+                                    .frame(width: 20.0)
+                            }
+                            Button(action: goHome) {
+                                Image(systemName: "house.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.black)
+                                    .frame(width: 20.0)
+                            }
+                        }
+
+            .aspectRatio(contentMode: .fit)
         }
     }
     
+    func goBack() {
+        webView.goBack()
+    }
+    
+    func goForward() {
+        webView.goForward()
+    }
+    
+    func goHome() {
+        webView.load(URLRequest(url: startURL))
+    }
+}
+    struct MoodleView: View {
+        @State private var webView = WKWebView()
+        let startURL = URL(string: "https://moodle.oszimt.de/")!
+        var body: some View {
+            VStack{
+                WebView(webView: $webView, request: URLRequest(url: startURL))
+                HStack {
+                    Spacer()
+                                Button(action: goBack) {
+                                    Image(systemName: "arrowshape.turn.up.left.circle")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .foregroundColor(Color.white)
+                                        .background(Color.black)
+                                        .frame(width: 20.0)
+                                }
+                                Button(action: goForward) {
+                                    Image(systemName: "arrowshape.turn.up.right.circle")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .foregroundColor(Color.white)
+                                        .background(Color.black)
+                                        .frame(width: 20.0)
+                                }
+                                Button(action: goHome) {
+                                    Image(systemName: "house.circle")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .foregroundColor(Color.white)
+                                        .background(Color.black)
+                                        .frame(width: 20.0)
+                                }
+                            }
 
+                .aspectRatio(contentMode: .fit)
+            }
+        }
         
-
+        func goBack() {
+            webView.goBack()
+        }
+        
+        func goForward() {
+            webView.goForward()
+        }
+        
+        func goHome() {
+            webView.load(URLRequest(url: startURL))
+        }
+    }
 struct TimeTableView: View {
-    @State private var webAddress = "https://mese.webuntis.com/WebUntis/monitor?school=OSZ%20IMT&simple=2&type=1&monitorType=tt&name=ITA%2012"
+    @State private var webView = WKWebView()
+    let startURL = URL(string: "https://mese.webuntis.com/WebUntis/monitor?school=OSZ%20IMT&simple=2&type=1&monitorType=tt&name=ITA%2012")!
     var body: some View {
         VStack{
-            WebView(request: URLRequest(url: URL(string: webAddress)!))
-     /*       HStack{
+            WebView(webView: $webView, request: URLRequest(url: startURL))
+            HStack {
                 Spacer()
-                Button(action: {
-                    webViewStateModel.pageTitle = webAddress
-                    webViewStateModel.goToPage = true
-                }, label: {
-                    Image(systemName: "house.circle")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color("AccentColor"))
-                        .frame(width: 20.0)
-                })
-                .font(.title)
-                Button(action: {
-                    webViewStateModel.goBack = true
-                }, label: {
-                    Image(systemName: "arrow.backward")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color("AccentColor"))
-                        .frame(width: 20.0)
-                })
-                Button(action: {
-                    webViewStateModel.goForward = true
-                }, label: {
-                    Image(systemName: "arrow.forward")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color("AccentColor"))
-                        .frame(width: 20.0)
-                })
-            }
-            .aspectRatio(contentMode: .fit) */
+                            Button(action: goBack) {
+                                Image(systemName: "arrowshape.turn.up.left.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.black)
+                                    .frame(width: 20.0)
+                            }
+                            Button(action: goForward) {
+                                Image(systemName: "arrowshape.turn.up.right.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.black)
+                                    .frame(width: 20.0)
+                            }
+                            Button(action: goHome) {
+                                Image(systemName: "house.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.black)
+                                    .frame(width: 20.0)
+                            }
+                        }
+
+            .aspectRatio(contentMode: .fit)
         }
+    }
+    
+    func goBack() {
+        webView.goBack()
+    }
+    
+    func goForward() {
+        webView.goForward()
+    }
+    
+    func goHome() {
+        webView.load(URLRequest(url: startURL))
     }
 }
 struct OSZimtView: View {
-    @State private var webAddress = "https://oszimt.de"
+    @State private var webView = WKWebView()
+    let startURL = URL(string: "https://oszimt.de")!
     var body: some View {
         VStack{
-            WebView(request: URLRequest(url: URL(string: webAddress)!))
-          /*  HStack{
+            WebView(webView: $webView, request: URLRequest(url: startURL))
+            HStack {
                 Spacer()
-                Button(action: {
-                    webViewStateModel.pageTitle = webAddress
-                    webViewStateModel.goToPage = true
-                }, label: {
-                    Image(systemName: "house.circle")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color("AccentColor"))
-                        .frame(width: 20.0)
-                })
-                .font(.title)
-                Button(action: {
-                    webViewStateModel.goBack = true
-                }, label: {
-                    Image(systemName: "arrow.backward")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color("AccentColor"))
-                        .frame(width: 20.0)
-                })
-                Button(action: {
-                    webViewStateModel.goForward = true
-                }, label: {
-                    Image(systemName: "arrow.forward")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .foregroundColor(Color("AccentColor"))
-                        .frame(width: 20.0)
-                })
-            }
-            .aspectRatio(contentMode: .fit) */
+                            Button(action: goBack) {
+                                Image(systemName: "arrowshape.turn.up.left.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.black)
+                                    .frame(width: 20.0)
+                            }
+                            Button(action: goForward) {
+                                Image(systemName: "arrowshape.turn.up.right.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.black)
+                                    .frame(width: 20.0)
+                            }
+                            Button(action: goHome) {
+                                Image(systemName: "house.circle")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .foregroundColor(Color.white)
+                                    .background(Color.black)
+                                    .frame(width: 20.0)
+                            }
+                        }
+
+            .aspectRatio(contentMode: .fit)
         }
+    }
+    
+    func goBack() {
+        webView.goBack()
+    }
+    
+    func goForward() {
+        webView.goForward()
+    }
+    
+    func goHome() {
+        webView.load(URLRequest(url: startURL))
     }
 }
 
